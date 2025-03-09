453593cf72464dfdb63270ff7713177c5267129c
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-03-09 20:54:55 +0530

    Added useEffect in ThemeToggle.tsx

--- apps/web/components/ThemeToggle.tsx
+++ apps/web/components/ThemeToggle.tsx
@@ -3,20 +3,27 @@
 import { useTheme } from "next-themes";
 import { Switch } from "@/components/ui/switch";
 import { Sun, Moon } from "lucide-react";
+import { useState, useEffect } from "react";
+
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
-  const isDark = theme === "dark";
+  const [checked, setChecked] = useState(false);
+  
+  useEffect(()=>{
+    const isDark = theme === "dark";
+    setChecked(isDark)
+  },[]);
 
   return (
     <div className="flex items-center gap-2">
-      <Sun className={`h-5 w-5 ${!isDark ? "text-pink-500" : ""}`} />
+      <Sun className={`h-5 w-5 ${!checked ? "text-pink-500" : ""}`} />
       <Switch
         className="data-[state=checked]:bg-pink-500 data-[state=unchecked]:bg-input"
-        checked={isDark}
-        onCheckedChange={() => setTheme(isDark ? "light" : "dark")}
+        checked={checked}
+        onCheckedChange={() => setTheme(checked ? "light" : "dark")}
       />
-      <Moon className={`h-5 w-5 ${isDark ? "text-pink-500" : ""}`} />
+      <Moon className={`h-5 w-5 ${checked ? "text-pink-500" : ""}`} />
     </div>
   );
 }
